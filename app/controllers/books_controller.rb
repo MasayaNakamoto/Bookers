ab5b4fe693d845912ccba85901086c6e0758734d
MasayaNakamoto <daigoroh67@yahoo.co.jp>
2021-06-10 06:28:16 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,55 +1,51 @@
 class BooksController < ApplicationController
-
-
-
-
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
   end
 
-
   def new
     @book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-   if  book.save
-     flash[:notice] = 'Book was successfully created.'
-     redirect_to book_path(book.id)
-    else
-      render :new
-    end
-
+    @book = Book.new(book_params)
+      if  @book.save
+        flash[:notice] = 'Book was successfully created.'
+        redirect_to book_path(@book.id)
+      else
+        render :new
+      end
   end
-
 
   def edit
     @book = Book.find(params[:id])
   end
 
   def update
-    book = Book.find(params[:id])
-   if  book.update(book_params)
-       flash[:notice] = 'Book was successfully updated.'
-       redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = 'Book was successfully updated.'
+      redirect_to book_path(@book)
     else
       render :edit
     end
   end
 
   def destroy
-     book = Book.find(params[:id])
+    book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
 
   private
   def book_params
-    params.permit(:title,:body)
+    # {"book"=> {"title"=> "", "body"=> ""}}
+    params.require(:book).permit(:title,:body)
+    # {"title"=> "", "body"=> ""}
   end
 end
