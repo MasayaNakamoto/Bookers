5b032f83a538dc3bf968a2cdff78fae5f38271d1
MasayaNakamoto <daigoroh67@yahoo.co.jp>
2021-06-23 08:10:55 +0000

    fukusyuu

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,7 +30,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     if @book.update(book_params)
       flash[:notice] = 'Book was successfully updated.'
-      redirect_to books_path(@book)
+      redirect_to book_path(@book)
     else
       render :edit
     end
